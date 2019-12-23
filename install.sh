e05c710ac09774e1fdf0fc417366644435c2a6e9
secrectvn <quoclong.hacker1412@gmail.com>
2019-12-23 20:18:02 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 #Create brand: Day of Week
+read -p  "Select the git project directory you want to backup: (/home/user/project/)" folder
+cd $folder
 dayofweek='Mon Tue Thu Fri Sat Sun'
 for dayofweek in $dayofweek
 do
